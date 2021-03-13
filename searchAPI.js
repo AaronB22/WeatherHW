1e6437e1d20ea059a378a954132052a0b65af573
Aaron <aaronthesnowboarder@live.com>
2021-03-12 21:51:06 -0800

    added logic to the main weather display. Reformatted the 5 day forecast div elements so they don't overlap.

--- searchAPI.js
+++ searchAPI.js
@@ -1,53 +1,10 @@
-// const apiURLtest = 'https://api.github.com/users/AaronB22/repos'
 
-// fetch(apiURLtest)
-//     .then(function (response){
-//         if (response.ok){
-//             console.log(response);
-//             response.json().then(function(data){
-//                 console.log(data);
-//             })
-//         }
-//     })
-
- const userSearch =document.querySelector('.userInput')
- 
-         const weatherKey="&appid=c80f9a4fde963804b68e5e32b0e539de";
-
-
-    // class weatherFetch{
-    //     async getWeather(userInput){
-            
-    //         const response= await fetch(
-    //             // 'http://api.openweathermap.org/data/2.5/weather?q='+userInput,'&appid=c80f9a4fde963804b68e5e32b0e539de'
-    //             'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c80f9a4fde963804b68e5e32b0e539de'
-    //         );
-    //             const data= await response.json();
-    //             console.log(data);
-    //             return data;
-
-
-
-    //     }
-    // }
-
-
-// function getWeather(){
-//     console.log(userSearch.value)
-//     fetch(weahterURL)
-//     .then(function(response){
-//         if(response.ok){
-//             console.log(response)
-//             response.json().then(function(data){
-//                 console.log(data)
-//                 return data;
-//             })
-            
-//         }
-//     })}
-    
+                    
+                    const userSearch =document.querySelector('.userInput')
+                    
+                            const weatherKey="c80f9a4fde963804b68e5e32b0e539de";
     function getWeather(){
-        const geoCodingUrl='http://api.openweathermap.org/geo/1.0/direct?q='+userSearch.value+'&limit&appid'+ weatherKey   
+        const geoCodingUrl='http://api.openweathermap.org/geo/1.0/direct?q='+userSearch.value+'&limit&appid='+ weatherKey   
         fetch(geoCodingUrl)
         .then(function (response){
             
@@ -57,24 +14,27 @@
                 
                
                 let lat= data[0].lat;
-                let lon= data[0].lon
-                console.log([lat, lon])
-                const weatherURL= 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+ weatherKey
+                let lon= data[0].lon;
+               
+                const weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial'+'&appid='+ weatherKey
                 console.log(weatherURL)
-                .then(fetch(weatherURL).then (function (weatherResponse){
+               
+                fetch(weatherURL)
+               
+                    .then (function (weatherResponse){
                     weatherResponse.json()
                     .then(function(weatherData){
                         console.log(weatherData)
-                    })
-                }))
-            })
-            
-        }
-        
-            
-        )}
-   
-   function testCords(){
-   
-  
-}
+                        const temp = weatherData.daily[0].temp.day
+                        document.getElementById('tempMain').textContent= temp + "F"
+                        document.getElementById('humMain').textContent='humidity '+ weatherData.daily[0].humidity +"%"
+                        document.getElementById('windMain').textContent="Wind Speed " +weatherData.daily[0].wind_speed +"mph"
+                        document.getElementById('uvMain').textContent="UVI =" +weatherData.daily[0].uvi
+                       
+                        console.log(weatherURL)
+                        
+                        
+               
+    })}
+        )})})}
+           
