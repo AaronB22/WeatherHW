e727feefeb648abe3b274829d4e6823415a7427f
AaronB22 <77556261+AaronB22@users.noreply.github.com>
2021-03-15 22:29:50 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const searchBtnHP = document.querySelector('#searchBtnHp');
-searchBtnHP.addEventListener("click", searchFunction)
-
-
-function searchFunction(){
-    getWeather()
-    // window.location.href='weather.html'
-}
-
